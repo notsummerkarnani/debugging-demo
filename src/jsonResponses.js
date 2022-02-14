31600c1cc37e030604495903d9932e56ea67cb2c
notsummerkarnani <93565277+notsummerkarnani@users.noreply.github.com>
2022-02-14 14:31:01 -0500

    complete

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -3,53 +3,52 @@
 const users = {};
 
 const respondJSON = (request, response, status, object) => {
-  response.writeHead(status, { 'Content-Type': 'application/json' });
-  response.write(JSON.stringify(object));
-  response.end();
+    response.writeHead(status, { 'Content-Type': 'application/json' });
+    response.write(JSON.stringify(object));
+    response.end();
 };
 
 const respondJSONMeta = (request, response, status) => {
-  response.writeHead(status, { 'Content-Type': 'application/json' });
-  response.end();
+    response.writeHead(status, { 'Content-Type': 'application/json' });
+    response.end();
 };
 
 const getUsers = (request, response) => {
-  const responseJSON = {
-    users,
-  };
+    const responseJSON = {
+        users,
+    };
 
-  respondJSON(request, response, 200, responseJSON);
+    respondJSON(request, response, 200, responseJSON);
 };
 
 const addUser = (request, response, body) => {
-  const responseJSON = {
-    message: 'Name and age are both required.',
-  };
+    const responseJSON = {
+        message: 'Name and age are both required.',
+    };
 
-  if (!body.name || !body.age) {
-    responseJSON.id = 'missingParams';
-    return respondJSON(request, response, 400, responseJSON);
-  }
+    if (!body.name || !body.age) {
+        responseJSON.id = 'missingParams';
+        return respondJSON(request, response, 400, responseJSON);
+    }
 
-  let responseCode = 204;
-  if (!users[body.name]) {
-    responseCode = 201;
-    users[body.name] = {};
-  }
+    let responseCode = 204;
+    if (!users[body.name]) {
+        responseCode = 201;
+        users[body.name] = {};
+    }
 
-  users[body.name].name = body.name;
-  users[body.name].age = body.age;
+    users[body.name].name = body.name;
+    users[body.name].age = body.age;
 
-  if (responseCode === 201) {
-    responseJSON.message = 'Created Successfully';
-    respondJSON(request, response, responseCode, responseJSON);
-    return respondJSON(request, response, responseCode, responseJSON);
-  }
+    if (responseCode === 201) {
+        responseJSON.message = 'Created Successfully';
+        return respondJSON(request, response, responseCode, responseJSON);
+    }
 
-  return respondJSONMeta(request, response, responseCode);
+    return respondJSONMeta(request, response, responseCode);
 };
 
 module.exports = {
-  getUsers,
-  addUser,
+    getUsers,
+    addUser,
 };
